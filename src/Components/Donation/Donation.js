4d95e2b34cb7d724364a043a16dcbcbafd953b64
mathieumarcelino <marcelinomathieu91@gmail.com>
2022-07-07 14:54:10 +0200

    Add donation

---
+++ src/Components/Donation/Donation.js
@@ -0,0 +1,18 @@
+// ----- IMPORT -----
+import React from 'react';
+import './Donation.css';
+// ------------------
+
+
+const Donation = () => {
+
+  return (
+    <section className="donation">
+      <a class="btn" href="https://donner.pse.ong/fr/donner/~mon-don"><span class="text-btn">Je donne</span></a>
+      <a class="btn" href="https://donner.pse.ong/fr/parrainer/~mon-don"><span class="text-btn">Je paraine</span></a>
+    </section>
+  )
+
+}
+
+export default Donation;
